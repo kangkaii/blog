8d9b09563b886bc2b522f303abfdf8e111b74d02
kangkai <644192628@qq.com>
2018-04-20 14:43:55 +0800

    一堆工具类

--- src/main/java/com/k/blog/BlogApplication.java
+++ src/main/java/com/k/blog/BlogApplication.java
@@ -1,15 +1,35 @@
 package com.k.blog;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 //扫描mapper包下
 @MapperScan("com.k.blog.mapper")
+//开启事务支持
+@EnableTransactionManagement
 public class BlogApplication {
+	@Bean(initMethod = "init", destroyMethod = "close")
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource dataSource() {
+		return new DruidDataSource();
+	}
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(dataSource());
+	}
+
 	private static final Logger logger = LoggerFactory.getLogger(BlogApplication.class);
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/k/blog/web/AttachController.java
@@ -0,0 +1,46 @@
+package com.k.blog.web;
+
+import com.k.blog.utils.TaleUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 附件管理
+ * <p>
+ * Created by 13 on 2017/2/21.
+ */
+@Controller
+@RequestMapping("admin/attach")
+public class AttachController extends BaseController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AttachController.class);
+
+    public static final String CLASSPATH = TaleUtils.getUplodFilePath();
+
+
+
+    /**
+     * 附件页面
+     *
+     * @param request
+     * @param page
+     * @param limit
+     * @return
+     */
+    @GetMapping(value = "")
+    public String index(HttpServletRequest request, @RequestParam(value = "page", defaultValue = "1") int page,
+                        @RequestParam(value = "limit", defaultValue = "12") int limit) {
+       /* PageInfo<AttachVo> attachPaginator = attachService.getAttachs(page, limit);
+        request.setAttribute("attachs", attachPaginator);
+        request.setAttribute(Types.ATTACH_URL.getType(), Commons.site_option(Types.ATTACH_URL.getType(), Commons.site_url()));
+        request.setAttribute("max_file_size", WebConst.MAX_FILE_SIZE / 1024);*/
+        return "admin/attach";
+    }
+
+
+
+}
